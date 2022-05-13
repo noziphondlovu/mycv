b8be33eccaa6ed414eecc779fbdecb209c54d0d7
Samkelisiwe <noziphondlovu910@gmail.com>
2022-05-13 09:52:59 +0200

    recopy new bootstrap template

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 /*!
-* Start Bootstrap - Agency v7.0.11 (https://startbootstrap.com/theme/agency)
+* Start Bootstrap - Grayscale v7.0.5 (https://startbootstrap.com/theme/grayscale)
 * Copyright 2013-2022 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-grayscale/blob/master/LICENSE)
 */
 //
 // Scripts
